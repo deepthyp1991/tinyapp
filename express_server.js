eebda43814c22f39044d5b161696fb2ab1b0b9a5
deepthyp1991 <deepthyp1991@gmail.com>
2020-12-07 20:39:45 +0000

    Adding routes to the server

--- express_server.js
+++ express_server.js
@@ -7,9 +7,11 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 app.get("/", (req,res) => {
   res.send("Hello");
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(port, () => {
